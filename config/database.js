dacdaf27e84be52f6e1b9ed888968f3d8b03f371
Sebastian Buritica <jhoseburitica40@gmail.com>
2025-03-05 10:37:36 -0500

    melo

--- config/database.js
+++ config/database.js
@@ -4,9 +4,25 @@ const url = process.env.URI_MONGO
 console.log(url)
 //utilizamos connect de mongoose
 
+
+
+//conexion asincrona
 mongoose.connect(url)
 .then(() => console.log("conexion mela"))
 .catch(error => console.log(error))
 
 
+//conexion sincrona
+// async function connectDB(){
+//    try {
+
+//     await mongoose.connect(process.env.URI_MONGO)
+//     console.log("conexion mela")
+    
+//    } catch (error) {
+//       console.log(error)
+//    }
+// }
+
+// connectDB()
 
